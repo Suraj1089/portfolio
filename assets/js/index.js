89c00fa98d976bac632a06bd1f9b1ed076f68218
Suraj Pisal <surajpisal113@gmail.com>
2022-11-26 20:15:23 +0530

    contact form setup

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+var templateParams = {
+    name: 'James',
+    notes: 'Check this out!'
+};
+ 
+emailjs.sendForm('service_0od9jlw', 'template_cqytes3', templateParams) //use your Service ID and Template ID
+    .then(function(response) {
+       console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+       console.log('FAILED...', error);
+    });
