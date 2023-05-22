72fda053f9dfb37b05ca30f5f4dce9e88c1dbc95
Suraj Pisal <surajpisal113@gmail.com>
2023-05-22 15:57:53 +0530

    color change in header

--- main.py
+++ main.py
@@ -14,3 +14,7 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run('main:app',host='0.0.0.0',port=8000,reload=True)
