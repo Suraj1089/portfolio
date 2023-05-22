a7fe0e6f40220729a34fb525b892875343dbfe8b
Suraj Pisal <surajpisal113@gmail.com>
2023-05-22 21:56:41 +0530

    change in header color

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ templates = Jinja2Templates(directory="templates")
 async def read_item(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+@app.post('/message')
+def send_message(name: str, email: str, subject: str,message_body: str):
+    return {"response":"message sent"}
+
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run('main:app',host='0.0.0.0',port=8000,reload=True)
